a38d78dd05c488322229ceecb4ca77d2e16e1e3e
Allan <allan.loveforfood@gmail.com>
2019-01-07 17:53:14 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,25 +23,53 @@ var cards = [
 
 var cardsInPlay = [];
 
-var checkForMatch = function() {
-	if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry try again.");
-	}	
-  }
-}
 
-var flipCard = function(cardId) {
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-cardsInPlay.push(cards[cardId].rank);
-checkForMatch();
+var score = Number([0]);
+
+document.querySelector('#score').textContent = score;
+
+
+
+var checkForMatch = function() {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	score += 1;
+	document.querySelector('#score').textContent = score;
+	document.querySelector('#resetBtn').addEventListener('click', resetGame);
+
+	} else {
+	alert("Sorry try again.");
+	} 
+
 };
 
-flipCard(0);
-flipCard(2);
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute("src", cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
+}
+};
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+var resetGame = function() {
+	console.log('not sure how to reset everything');
+};
+
+
+
+
+createBoard();
 
 
